1052999f3063480ddd4868d72843278c50364662
semliko <tokvbok@gmail.com>
2020-12-10 15:27:41 +0000

    math functions

---
+++ 2-lesson-homework/mathFunctions.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+	result = a + b;
+	return result;
+}
+
+function deduction(a, b) {
+	result = a - b;
+	return result;
+}
+
+function multiply(a, b) {
+	rusult = a * b;
+	return result;
+}
+
+function divide(a, b) {
+	result a / b;
+	return result;
+}
